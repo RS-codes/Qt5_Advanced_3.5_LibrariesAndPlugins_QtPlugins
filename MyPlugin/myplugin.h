9b264b2ce3b4e5d4cd1ea9408bd338bb7e8b8eab
RS-codes <RS-codes@github.com>
2021-08-15 09:49:14 +0530

    SourceCode added

---
+++ MyPlugin/myplugin.h
@@ -0,0 +1,27 @@
+#ifndef MYPLUGIN_H
+#define MYPLUGIN_H
+
+#include <QGenericPlugin>
+#include <QDebug>
+#include "itest.h"
+
+class MyPlugin : public QGenericPlugin, iTest
+{
+    Q_OBJECT
+    Q_INTERFACES(iTest)
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QGenericPluginFactoryInterface" FILE "MyPlugin.json")
+#endif // QT_VERSION >= 0x050000
+
+public:
+    explicit MyPlugin(QObject *parent = nullptr);
+
+private:
+    QObject *create(const QString &name, const QString &spec) override;
+
+    // iTest interface
+public:
+    void test() override;
+};
+
+#endif // MYPLUGIN_H
